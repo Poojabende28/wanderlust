0386d618491abd50ab96c39f2f064db8f917eecf
Pooja Bende <poojabende334@gmail.com>
2026-06-23 22:32:47 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,129 @@
+if(process.env.NODE_ENV != "production") {
+require("dotenv").config();
+}
+
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const path = require("path");
+const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js"); // ✅ FIXED (User capital)
+
+const listingRouter = require("./routes/listing.js");
+const reviewsRouter = require("./routes/reviews.js");
+const userRouter = require("./routes/user.js");
+
+// ================= SETTINGS =================
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.engine("ejs", ejsMate);
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+
+const sessionOptions = {
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
+
+// ================= ROUTES =================
+// app.get("/", (req, res) => {
+//   res.send("Hi, I am root");
+// });
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+  res.locals.currUser = req.user;
+  next();
+});
+
+// ================= DB CONNECT =================
+main()
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+}
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  next();
+});
+
+// ================= DEMO USER =================
+// app.get("/demouser", async (req, res) => {
+//   try {
+//     let fakeUser = new User({
+//       email: "student@gmail.com",
+//       username: "delta-student",
+//     });
+
+//     let registeredUser = await User.register(fakeUser, "123456");
+
+//     res.send(registeredUser);
+
+//   } catch (e) {
+//     res.send("User already exists");
+//   }
+// });
+
+// Listings Routes // Reviews Routes
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewsRouter);
+app.use("/", userRouter);
+
+// ================= 404 =================
+app.use((req, res, next) => {
+  next(new ExpressError(404, "Page Not Found!"));
+});
+
+// ================= ERROR HANDLER =================
+app.use((err, req, res, next) => {
+  console.log(err);
+
+  if (err.name === "CastError") {
+    err = new ExpressError(400, "Invalid ID");
+  }
+
+  let { statusCode = 500, message = "Something went wrong!" } = err;
+
+  res.status(statusCode).send(message);
+});
+
+// ================= SERVER =================
+app.listen(8080, () => {
+  console.log("Server listening on port 8080");
+});
---
+++ models/listing.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Review = require("./review.js");
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+
+  image: {
+    url: String,
+    filename: String,
+  },
+
+  price: Number,
+  location: String,
+  country: String,
+
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  category: {
+  type: String,
+  enum: ["trending", "rooms", "iconic_cities", "mountains", "castles", "pools", "camping", "farms", "arctic"]
+}
+});
+
+
+
+
+listingSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: { $in: doc.reviews }
+    });
+  }
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
